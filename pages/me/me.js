22b7cecb22bf12067290b5ec880c7a48ad8c222a
vizzk <992469572@qq.com>
2020-08-23 16:47:45 +0800

    头像改为易班

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,6 +5,7 @@ Page({
     userid:getApp().globalData.userId,
     name: '',
     id:"",
+    avatar:'',
   },
 
   onLoad:function(e){
@@ -14,14 +15,14 @@ Page({
       url: "http://liveforjokes.icu:8800/getStudentDetail",
       data:{id:data.userid},
     }).then(res=>{
-      this.setData({name:res.data.obj.name})
+      this.setData({name:res.data.obj.name,avatar:res.data.obj.yb_userhead})
     })
   },
-  onShow: function(){
+  /*onShow: function(){
     this.setData({
       avatar: wx.getStorageSync('avatar') || 'https://yunlaiwu0.cn-bj.ufileos.com/teacher_avatar.png',
     })
-  },
+  },*/
 
   navToIdentity:function(e){
     var userid = this.data.userid;
