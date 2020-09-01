5227b0ed4374701eb3a40bd7d1c488a07a08b647
Sun <Sun@qq.com>
2020-09-01 20:09:13 +0800

    第十次更改

--- pages/footprint/footprint.js
+++ pages/footprint/footprint.js
@@ -14,12 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var app = getApp();
-    let userId = app.globalData.userId;
-    this.setData({
-      userId
-    })
-    this.getBrowsed();
+
   },
 
   /**
@@ -33,7 +28,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var app = getApp();
+    let userId = app.globalData.userId;
+    this.setData({
+      userId
+    })
+    this.getBrowsed();
   },
 
   getBrowsed(){
@@ -49,5 +49,18 @@ Page({
       })
     })
       
+  },
+
+  goNext:function(e){
+    let num = e.currentTarget.dataset.type
+    let id  = e.currentTarget.dataset.id
+    console.log(e.currentTarget.dataset.type);
+    if(num == 1)wx.navigateTo({
+      url: "../leaving_publish/leaving_publish?activityId="+id
+    });
+    else if(num == 2)
+    wx.navigateTo({
+      url: "../prove/prove?certificateId="+id
+    });
   }
 })
